a09859e9bcd30bcc870c69c31e1e5ffac55dded8
Logan Torr <ltorr0401@gmail.com>
2025-08-07 22:06:57 -0500

    Calendar UI and tune up

--- GymRat-app/components/Profile/ProfileCalendar.jsx
+++ GymRat-app/components/Profile/ProfileCalendar.jsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions, useMemo, useState, useCallback } from 'react-native';
 import {Calendar, CalendarUtils} from 'react-native-calendars';
+
+const { width: screenWidth } = Dimensions.get('window');
+const INITIAL_DATE = '2024-11-06';
 
 
 const ProfileCalendar = () => {
+    const [selected, setSelected] = React.useState(INITIAL_DATE);
+
+    const getDate = (count)=> {
+        const date = new Date(INITIAL_DATE);
+        const newDate = date.setDate(date.getDate() + count);
+        return CalendarUtils.getCalendarDateString(newDate);
+    };
+
+    const onDayPress = React.useCallback((day) => {
+        setSelected(day.dateString);
+    }, []);
+
+    const marked = React.useMemo(() => {
+        return {
+        [getDate(-1)]: {
+            dotColor: '#6a5acd',
+            marked: true
+        },
+        [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedColor: '#6a5acd',
+            selectedTextColor: 'white'
+        }
+        };
+    }, [selected]);
+
     return (
         <Calendar
-            onDayPress={day => { console.log('day pressed', day)}}
+            onDayPress={onDayPress}
             onDayLongPress={day => { console.log('selected day', day)}}
-            monthFormat={'yyyy MM'}
+            monthFormat={'MMMM yyyy'} 
             hideArrows={false} //false is default
             hideExtraDays={true} //don't show days of other months
             // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
             // day from another month that is visible in calendar page. Default = false
             disableMonthChange={true}
             firstDay={1}
-            hideDayNames={true}
-            showWeekNumbers={true}
+            hideDayNames={false}
+            showWeekNumbers={false}
             onPressArrowLeft={subtractMonth => subtractMonth()}
             onPressArrowRight={addMonth => addMonth()}
-            enableSwipeMonths={true}
+            enableSwipeMonths={false}
+            markedDates={marked}
+            style={{width:screenWidth * 0.95, backgroundColor: 'white', borderRadius:10, borderWidth:6, borderColor:'#6a5acd'}}
         />
     )
 };
